89a93e28ceb4ed19f4a6fb879c3417d7a8d3755f
olamarvel <olatundemarvelousanthony@gmail.com>
2024-09-24 12:25:57 +0100

    changed apikey defualt field to undefined; also added the ability to determine conrrect path  in pkg and in developement

--- modules/config.js
+++ modules/config.js
@@ -1,19 +1,19 @@
-const fs = require('fs');
+const fs = require('fs');   
 const path = require('path');
 
-const CONFIG_PATH = path.resolve(__dirname, '../config.json');
-
+const CONFIG_PATH = path.resolve(process.pkg?process.cwd():__dirname, process.pkg?'config.json':'../config.json');
+// console.log(CONFIG_PATH)
 const loadConfig = () => {
     if (fs.existsSync(CONFIG_PATH)) {
         return JSON.parse(fs.readFileSync(CONFIG_PATH, 'utf8'));
     }
     return {
         interval: 60,
-        apiKey: 'your-api-key',
+        apiKey: undefined,
         schedule: '* * * * *',
         factType: 'general',
         cacheAmount: 10,
-        rateOfFetching: 10
+        rateOfFetching: 0.6
     };
 };
 
