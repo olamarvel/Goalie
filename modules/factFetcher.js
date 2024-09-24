625f4939a756aa2305674b9a24b975003ebc9051
olamarvel <olatundemarvelousanthony@gmail.com>
2024-09-24 12:20:49 +0100

    changed axios to the node-fecth package to adi building with pkg

--- modules/factFetcher.js
+++ modules/factFetcher.js
@@ -1,35 +1,45 @@
 const fs = require('fs');
 const path = require('path');
-const { default: axios } = require('axios');
+const fetch = require('node-fetch');
+const { isDifferenceMoreThanAWeek } = require('./utils');
 
-const FILE_PATH = path.resolve(__dirname, 'facts.json');
-
+const FILE_PATH = path.resolve(process.pkg?process.cwd():__dirname, '../facts.json');
 
 async function getFact(API_KEY) {
+
     try {
-        const response = await axios.get('https://api.api-ninjas.com/v1/facts', {
+        const response = await fetch('https://api.api-ninjas.com/v1/facts', {
             headers: { 'X-Api-Key': API_KEY },
         });
-        if (response.status !== 200 || !response.data[0]?.fact) {
-            console.log('Bad request:', response.status);
+
+        const data = await response.json();
+        if (!response.ok) {
+            console.log('Bad request:', data.error);
             return null;
         }
-        return response.data[0].fact;
+
+        if (!data[0]?.fact) {
+            return null;
+        }
+
+        return data[0].fact;
     } catch (error) {
         console.error('Error fetching fact:', error.message);
-        console.error(error)
         return null;
     }
 }
 
 function saveFact(fact, cacheAmount) {
+    if (fact === null) return;
     if (!fs.existsSync(FILE_PATH)) {
         fs.writeFileSync(FILE_PATH, JSON.stringify([]));
     }
     let facts = JSON.parse(fs.readFileSync(FILE_PATH, 'utf8'));
 
     // Filter out old facts
-    facts = facts.filter(storedFact => !isDifferenceMoreThanAWeek(storedFact.date, new Date()));
+    facts = facts.filter(
+        storedFact => !isDifferenceMoreThanAWeek(storedFact.date, new Date())
+    ).filter(storedFact => storedFact.fact);
 
     // Ensure the number of facts does not exceed cacheAmount
     if (facts.length >= cacheAmount) {
@@ -43,8 +53,6 @@ function saveFact(fact, cacheAmount) {
     fs.writeFileSync(FILE_PATH, JSON.stringify(facts, null, 2));
 }
 
-
-
 /**
  * Get a random fact from the saved facts in the JSON file.
  * @returns {string|null} - A random fact or null if no facts are available.
@@ -55,13 +63,13 @@ function getRandomSavedFact() {
             console.error('Facts file does not exist.');
             return null;
         }
-        
+
         const facts = JSON.parse(fs.readFileSync(FILE_PATH, 'utf8'));
         if (facts.length === 0) {
             console.error('No facts available.');
-            return null;
+            return 'Do you know that if you set a higher fetch chance on the dashboard, you get more fresh facts but also burn through your API key quickly?';
         }
-        
+
         const randomIndex = Math.floor(Math.random() * facts.length);
         return facts[randomIndex].fact;
     } catch (error) {
