946a330325ae6923e744af338dad49cc26b44897
olamarvel <olatundemarvelousanthony@gmail.com>
2024-09-24 11:18:41 +0100

    removed node notifier becuase of cyclic dependancies now event are added in the index.js file

--- modules/notifier.js
+++ modules/notifier.js
@@ -1,7 +1,6 @@
 const nodeNotifier = require('node-notifier');
 const openurl = require('openurl');
 const path = require('path');
-const { pauseCronJob, shutdownServer } = require('./cronManager');
 
 async function notifier(message, withoutActions) {
     return new Promise((resolve, reject) => {
@@ -37,23 +36,6 @@ async function notifier(message, withoutActions) {
             }
         );
     });
-}
-
-nodeNotifier.on('pause', pauseCronJob);
-nodeNotifier.on('shutdown', () => {
-    shutdownServer(server);
-});
-nodeNotifier.on('configure', configureSetting);
-
-async function configureSetting() {
-    notifier(
-        'Got it! You would like to configure the application. Redirecting you in 3 seconds...',
-        true
-    );
-    setTimeout(async () => {
-        await openurl.open('http://localhost:50805');
-        console.log('Successfully opened');
-    }, 3000);
 }
 
 module.exports = { notifier };
