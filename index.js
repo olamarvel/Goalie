e3b271d5265661536a85d795835a490f07dfb8e1
olamarvel <olatundemarvelousanthony@gmail.com>
2024-09-24 12:24:31 +0100

    Server file is no longer needed as the mechanism is now incorprated into the index.js file directly also moved the loging aspect to a  seprete file

--- index.js
+++ index.js
@@ -1,54 +1,131 @@
-const { loadConfig } = require('./modules/config');
-const { notifier } = require('./modules/notifier');
-const { getFact, getRandomSavedFact, saveFact } = require('./modules/factFetcher');
-const openurl = require('openurl');
-const { startCronJob, stopCronJob, shutdownServer, setNewInterval } = require('./modules/cronManager');
-const { secondsToCron } = require('./modules/utils');
-const { server } = require('./server');
-
-const fs = require("fs");
+const fs = require('fs');
 const path = require('path');
+const { notifier } = require('./modules/notifier');
+const nodeNotifier = require('node-notifier');
 
-const logFile = path.join(__dirname, 'log.txt');
-const logStream = fs.createWriteStream(logFile, { flags: 'a' });
+const CONFIG_PATH = path.join(process.pkg?process.cwd():__dirname, 'config.json');
 
-logStream.write(`Starting application at ${new Date().toISOString()}\n`);
 
-process.on('uncaughtException', (err) => {
-    logStream.write(`Uncaught Exception: ${err.stack}\n`);
+const {
+    getFact,
+    getRandomSavedFact,
+    saveFact,
+} = require('./modules/factFetcher');
+const openurl = require('openurl');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const { loadConfig } = require('./modules/config');
+const {
+    startCronJob,
+    shutdownServer,
+    restartCronJob,
+    pauseCronJob,
+} = require('./modules/cronManager');
+const { secondsToCron, shutdown } = require('./modules/utils');
+
+const app = express();
+const PORT = 50805;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+let config = loadConfig();
+
+app.get('/', () => {
+    path.join(__dirname, 'public', 'index.html');
 });
 
-process.on('exit', (code) => {
-    logStream.write(`Process exited with code: ${code}\n`);
+app.get('/welcome.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'welcome.html'));
 });
 
-const config = loadConfig();
+app.get('/config', (req, res) => {
+    res.json(config);
+});
 
-const { apiKey: API_KEY, cacheAmount } = config;
+app.post('/set-config', (req, res) => {
+    const {
+        interval,
+        apiKey,
+        schedule,
+        factType,
+        cacheAmount,
+        rateOfFetching,
+    } = req.body;
+    config = {
+        interval: Math.abs(parseInt(interval, 10)) || 60,
+        apiKey: apiKey || config.apiKey,
+        schedule: schedule || config.schedule,
+        factType: factType || config.factType,
+        cacheAmount: parseInt(cacheAmount, 10) || config.cacheAmount,
+        rateOfFetching: parseFloat(rateOfFetching) || config.rateOfFetching,
+    };
+    fs.writeFileSync(CONFIG_PATH, JSON.stringify(config, null, 2));
+    pauseCronJob();
+    startCronJob(secondsToCron(config.interval), main);
+    res.send('Configuration updated successfully.');
+});
 
-if (!API_KEY) {
+app.post('/pause', (req, res) => {
+    pauseCronJob();
+    res.send('Notifications paused.');
+});
+
+app.post('/restart', (req, res) => {
+    restartCronJob(main);
+    res.send('Notifications restarted.');
+});
+
+app.post('/shutdown', (req, res) => {
+    shutdownServer(server);
+    res.send('Server shutting down...');
+});
+
+const server = app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+    if (config.apiKey) startCronJob(secondsToCron(config.interval || 60), main);
+});
+if (!config.apiKey) {
     notifier('No API key detected, kindly configure', true);
     openurl.open('http://localhost:50805');
+}
+
+nodeNotifier.on('pause', pauseCronJob);
+nodeNotifier.on('shutdown', () => {
+    shutdownServer(server);
+});
+nodeNotifier.on('configure', configureSetting);
+
+function configureSetting() {
+    notifier(
+        'Got it! You would like to configure the application. Redirecting you in 3 seconds...',
+        true
+    );
+    setTimeout(async () => {
+        openurl.open('http://localhost:50805');
+        console.log('Successfully opened');
+    }, 3000);
 }
 
 async function main() {
     let fact;
     if (Math.random() <= (config.rateOfFetching || 0.5)) {
-        fact = await getFact(API_KEY);
-        saveFact(fact, cacheAmount);
+        fact = await getFact(config.apiKey);
+        saveFact(fact, config.cacheAmount);
     } else {
         fact = getRandomSavedFact();
     }
     if (!fact) {
-        notifier('Unable to fetch a new Fact, try checking your internet connection or checking your api key', true);
+        notifier(
+            'Unable to fetch a new Fact, try checking your internet connection or checking your api key',
+            true
+        );
         return;
     }
 
     const { response } = await notifier(fact, false);
     console.log(response, 'first');
 }
-
-logStream.write(`Application is running at ${new Date().toISOString()}\n`);
-startCronJob(secondsToCron(config.interval || 60), main);
-
-module.exports = { main };
+// startCronJob(secondsToCron(config.interval || 60), main);
+require('./log.js');
