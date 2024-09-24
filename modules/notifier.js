0f22eb6f20b4aa76aef6fe66ffd701e5e5b4307f
olamarvel <olatundemarvelousanthony@gmail.com>
2024-09-24 11:17:29 +0100

    added path to custom Snoretoast in build version as pkg does not bundle snoretoast by defination making the app only ofr windows 10 and above

--- modules/notifier.js
+++ modules/notifier.js
@@ -5,7 +5,17 @@ const { pauseCronJob, shutdownServer } = require('./cronManager');
 
 async function notifier(message, withoutActions) {
     return new Promise((resolve, reject) => {
-        nodeNotifier.notify(
+        const notifier = process.pkg
+            ? new nodeNotifier.WindowsToaster({
+                  withFallback: false, // Use Growl Fallback if <= 10.8
+                  customPath: path.join(
+                      path.dirname(process.execPath),
+                      'path-to-executable/notifier/snoretoast-x64.exe'
+                  ), // Relative/Absolute path to binary if you want to use your own fork of terminal-notifier
+              })
+            : nodeNotifier;
+
+        notifier.notify(
             {
                 'app-name': 'Fun fact Notifier by Olamarvel',
                 title: withoutActions ? 'Info' : 'Here is a fun fact for you.',
