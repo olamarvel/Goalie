705d794a1beac3b6317d270bc28936cc2d894021
olamarvel <olatundemarvelousanthony@gmail.com>
2024-10-03 19:01:30 +0100

    added demo configuration

---
+++ config.js
@@ -0,0 +1,6 @@
+const e ={
+    Matchtowatch: 7230,//matchid
+    onlyshowImportansdetais: true,// show only important deatils
+    internalrefreashrate: 60,// rate of checking for new update
+    showImageWherePosible: true,//wether to show images in notification
+}
